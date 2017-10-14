4f3037ef300f9c2387881d67205cd1ccf8b1c64e
zied hajsalah <ziedhajsalah@gmail.com>
2017-10-14 15:06:39 +0100

    ssr and refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
 const config = {
-  entry: ['babel-polyfill', './lib/components/Index.js'],
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'lib'),
+      path.resolve(__dirname, 'node_modules'),
+    ],
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }],
